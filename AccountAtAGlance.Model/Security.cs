edfb0b87e5ddcac05b5b4f0733f0782d6cf39306
rolypc87 <rolypc87@gmail.com>
2018-01-29 12:43:00 -0500

    Created classes DataPoint, Security adn Stock

--- AccountAtAGlance.Model/Security.cs
+++ AccountAtAGlance.Model/Security.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace AccountAtAGlance.Model
 {
-	public  class Security
+	public  abstract class Security
 	{
+		// Primitive Properties
+		public int Id { get; set; }
+		public decimal Change { get; set; }
+		public decimal PercentageChange { get; set; }
+		public decimal Last { get; set; }
+		public decimal Shares { get; set; }
+		public DateTime RetrivalDateTime { get; set; }
+		public string Company { get; set; }
+
+		//Additional Properties, not in DB
+		public List<DataPoint> DataPoints { get; set; }
 	}
 }
---
+++ AccountAtAGlance.Model/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountAtAGlance.Model
+{
+	public class Stock : Security
+	{
+		//Primitive Properties
+		public decimal DayHigh { get; set; }
+		public decimal DayLow { get; set; }
+		public decimal Divided { get; set; }
+		public decimal Open { get; set; }
+		public decimal Volume { get; set; }
+		public decimal YearHigh { get; set; }
+		public decimal YearLow { get; set; }
+		public decimal AverageVolume { get; set; }
+		public decimal MarketCap { get; set; }
+		public int ExchangeId { get; set; }
+		
+		//Navigation Properties
+		public virtual Exchange Exchange { get; set; }
+	}
+}
